ec49040fe5d3e4967893a4c4925925044dbc2d53
yash <yashsharma11@protonmail.com>
2026-06-01 17:29:05 +0530

    fixed the mobile view

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,13 +6,13 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="container">
         <NavLink to="/" className="navbar-brand">Inventory</NavLink>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <nav>
+        <div className="navbar-right">
+          <div className="navbar-links">
             <NavLink to="/" end>Dashboard</NavLink>
             <NavLink to="/products">Products</NavLink>
             <NavLink to="/customers">Customers</NavLink>
             <NavLink to="/orders">Orders</NavLink>
-          </nav>
+          </div>
           <ThemeToggle />
         </div>
       </div>
